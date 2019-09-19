1027c22cca8447235e05a453755d51b7d39f78c2
Trey <cooper6101@gmail.com>
2019-09-19 16:43:34 -0400

    added user model

---
+++ models/post.js
@@ -0,0 +1,10 @@
+// Post
+// -title -string
+// -price - string
+// -description - string
+// -images - array of strings
+// -location - string
+// -lat - number
+// -lng - number
+// -author - object id (ref User)
+// -reviews - array of objects
