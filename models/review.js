8b011cca6179c9f66fa34e18fa15d2409976dfab
Trey <cooper6101@gmail.com>
2019-09-20 07:37:30 -0400

    added post, user, review models

--- models/review.js
+++ models/review.js
@@ -1,3 +1,12 @@
-// Review
-// -body -string
-// -author -object id (ref User)
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    body: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
