ffe8c437e67492b3401eaf81dc36dd175ad3c5b3
Trey <cooper6101@gmail.com>
2019-09-21 22:18:10 -0400

    finished building post routes and controllers

--- controllers/posts.js
+++ controllers/posts.js
@@ -36,5 +36,11 @@ module.exports = {
     async postUpdate(req, res, next) {
         let post = await Post.findByIdAndUpdate(req.params.id, req.body.post);
         res.redirect(`/posts/${post.id}`);
+    },
+
+    // DELETE posts destroy /posts/:id
+    async postDestroy(req, res, next) {
+        await Post.findByIdAndRemove(req.params.id);
+        res.redirect('/posts');
     }
 }
