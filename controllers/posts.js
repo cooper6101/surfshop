450bca828d28baaf244aea117a8b2c873414da58
Trey <cooper6101@gmail.com>
2019-09-22 00:09:46 -0400

    added image upload with cloudinary

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,10 @@
 const Post = require('../models/post');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name: 'dmcponkuf',
+    api_key: '534365149354957',
+    api_secret: process.env.CLOUDINARY_SECRET
+});
 
 module.exports = {
     // GET /posts
@@ -14,6 +20,14 @@ module.exports = {
 
     //POST Create /posts
     async postCreate(req, res, next) {
+        req.body.post.images = [];
+        for(const file of req.files) {
+            let image = await cloudinary.v2.uploader.upload(file.path);
+            req.body.post.images.push({
+                url: image.secure_url,
+                public_id: image.public_id
+            });
+        }
         //use req.body to create a new post
         let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
