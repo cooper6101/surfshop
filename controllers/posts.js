d2669e1ae508f61b73232ec85807944c861c6a9d
Trey <cooper6101@gmail.com>
2019-09-21 21:24:11 -0400

    added posts controllers and updated post routes

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,28 @@
 const Post = require('../models/post');
 
 module.exports = {
+    // GET /posts
     async getPosts(req, res, next) {
         let posts = await Post.find({});
         res.render('posts/index', { posts });
+    },
+
+    //GET /posts/new
+    newPost(req, res, next) {
+        res.render('posts/new')
+    },
+
+    //POST Create /posts
+    async createPost(req, res, next) {
+        //use req.body to create a new post
+        let post = await Post.create(req.body);
+        res.redirect(`/posts/${post.id}`);
+    },
+
+    //GET posts show /posts/:id
+    async showPost(req, res, next) {
+        //find posts by id
+        let post = await Post.findById(req.params.id);
+        res.render('posts/show', { post });
     }
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const { errorHandler } =require('../middleware');
-const { getPosts } = require('../controllers/posts');
+const { 
+    getPosts, 
+    newPost, 
+    createPost,
+    showPost 
+} = require('../controllers/posts');
 
 /* GET posts index /posts */
 router.get('/', errorHandler(getPosts));
 
 /* GET posts new /posts/new */
-router.get('/new', (req, res, next) => {
-    res.send('GET /posts/new');
-});
+router.get('/new', newPost);
 
 /* POST posts create /posts */
-router.post('/', (req, res, next) => {
-    res.send('CREATE /posts');
-});
+router.post('/', errorHandler(createPost));
 
 /* GET posts show /posts/:id */
-router.get('/:id', (req, res, next) => {
-    res.send('GET /posts/:id');
-});
+router.get('/:id', errorHandler(showPost));
 
 /* GET posts edit /posts/:id/edit */
 router.get('/:id/edit', (req, res, next) => {
