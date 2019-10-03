44ab4858760519763692d43387e33900cdae1aef
Trey <cooper6101@gmail.com>
2019-10-03 08:39:40 -0400

    added mapbox clustering

--- controllers/posts.js
+++ controllers/posts.js
@@ -13,10 +13,11 @@ module.exports = {
     async postIndex(req, res, next) {
         let posts = await Post.paginate({}, {
             page: req.query.page || 1,
-            limit: 10
+            limit: 10,
+            sort: '-_id'
         });
         posts.page = Number(posts.page);
-        res.render('posts/index', { posts, title: 'Posts Index' });
+        res.render('posts/index', { posts, title: 'Posts Index', mapboxToken: process.env.MAPBOX_TOKEN });
     },
 
     //GET /posts/new
@@ -41,8 +42,10 @@ module.exports = {
         })
         .send();
         //use req.body to create a new post
-        req.body.post.coordinates = response.body.features[0].geometry.coordinates;
-        let post = await Post.create(req.body.post);
+        req.body.post.geometry = response.body.features[0].geometry;
+        let post = new Post(req.body.post);
+		post.properties.description = `<strong><a href="/posts/${post._id}">${post.title}</a></strong><p>${post.location}</p><p>${post.description.substring(0, 20)}...</p>`;
+		await post.save();
         req.session.success = 'Post created successfully!';
         res.redirect(`/posts/${post.id}`);
     },
@@ -109,13 +112,14 @@ module.exports = {
             limit: 1
         })
         .send();
-        post.coordinates = response.body.features[0].geometry.coordinates;
+        post.geometry = response.body.features[0].geometry;
         post.location = req.body.post.location;
     }
     // update the post with any new properties
     post.title = req.body.post.title;
     post.description = req.body.post.description;
     post.price = req.body.post.price;
+    post.properties.description = `<strong><a href="/posts/${post._id}">${post.title}</a></strong><p>${post.location}</p><p>${post.description.substring(0, 20)}...</p>`;
     // save the update post into the db
     post.save();
     // redirect to show page
