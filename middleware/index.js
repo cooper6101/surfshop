5e183ac1da28012b85f37de5b3a8533e1c345992
Trey <cooper6101@gmail.com>
2019-11-05 08:05:19 -0500

    added profile image feature

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,9 @@
 const Review = require('../models/review');
 const User = require('../models/user');
 const Post = require('../models/post');
+const { cloudinary } = require('../cloudinary');
 
-module.exports = {
+const middleware = {
     asyncErrorHandler: (fn) =>
         (req, res, next) => {
             Promise.resolve(fn(req, res, next))
@@ -42,6 +43,7 @@ module.exports = {
         res.locals.user = user;
         next();
       } else {
+        middleware.deleteProfileImage(req);
         req.session.error = 'Current password is not correct!';
         return res.redirect('/profile');
       }
@@ -52,6 +54,7 @@ module.exports = {
               passwordConfirmation
              } = req.body;
       if (newPassword && !passwordConfirmation) {
+        middleware.deleteProfileImage(req);
         req.session.error = 'Missing password confirmation!';
         return res.redirect('/profile');
       }       
@@ -61,11 +64,18 @@ module.exports = {
           await user.setPassword(newPassword);
           next();
         } else {
+          middleware.deleteProfileImage(req);
           req.session.error = 'New Passwords must match!';
           return res.redirect('/profile');
         }
       } else {
         next();
       }
+    },
+
+    deleteProfileImage: async req => {
+      if (req.file) await cloudinary.v2.uploader.destroy(req.file.public_id);
     }
-}
+};
+
+module.exports = middleware;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 const { 
   landingPage,
   getRegister, 
@@ -9,10 +12,11 @@ const {
   getLogout,
   getProfile,
   updateProfile } = require('../controllers');
-const { asyncErrorHandler,
-        isLoggedIn,
-        isValidPassword,
-        changePassword } = require('../middleware');
+const { 
+    asyncErrorHandler,
+    isLoggedIn,
+    isValidPassword,
+    changePassword } = require('../middleware');
 
 /* GET home/landing page. */
 router.get('/', asyncErrorHandler(landingPage));
@@ -21,7 +25,7 @@ router.get('/', asyncErrorHandler(landingPage));
 router.get('/register', getRegister);
 
 /* POST /register */
-router.post('/register', asyncErrorHandler(postRegister));
+router.post('/register', upload.single('image'), asyncErrorHandler(postRegister));
 
 /* GET /login */
 router.get('/login', getLogin);
@@ -38,6 +42,7 @@ router.get('/profile', isLoggedIn, asyncErrorHandler(getProfile));
 /* PUT /profile */
 router.put('/profile',
             isLoggedIn,
+            upload.single('image'),
             asyncErrorHandler(isValidPassword),
             asyncErrorHandler(changePassword),
             asyncErrorHandler(updateProfile)
